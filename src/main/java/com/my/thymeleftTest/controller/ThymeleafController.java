54556cec5c5adfa0fcac43e87529797efda5e39b
zzzmini <sm01079200887@gmail.com>
2025-08-04 16:52:18 +0900

    model

--- src/main/java/com/my/thymeleftTest/controller/ThymeleafController.java
+++ src/main/java/com/my/thymeleftTest/controller/ThymeleafController.java
@@ -6,6 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("basic")
 public class ThymeleafController {
@@ -20,6 +25,47 @@ public class ThymeleafController {
     @GetMapping("/variable")
     public String variable(Model model) {
         IveMember member = new IveMember();
+        member.setMemberName("안유진");
+        member.setAge(22);
+        member.setAddress("서울");
+        System.out.println(member);
+        model.addAttribute("member", member);
+
+        // 리스트 보내기
+        List<IveMember> memberList = new ArrayList<>();
+        IveMember iveMember = new
+                IveMember("이서", 23, "제주도");
+        memberList.add(iveMember);
+        model.addAttribute("list", memberList);
+
+        // Map으로 보내기
+        Map<String, IveMember> map = new HashMap<>();
+        IveMember hong = new
+                IveMember("홍길동", 30, "울릉도");
+        map.put("h", hong);
+
+        IveMember park = new
+                IveMember("박보검", 35, "인천");
+        map.put("p", park);
+        model.addAttribute("map", map);
         return "/basic/variable_object";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/my/thymeleftTest/dto/IveMember.java
+++ src/main/java/com/my/thymeleftTest/dto/IveMember.java
@@ -1,39 +1,15 @@
 package com.my.thymeleftTest.dto;
 
+import lombok.*;
+
+//@Getter
+//@Setter
+//@ToString
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IveMember {
     private String memberName;
     private int age;
     private String address;
-
-    public IveMember(){}
-
-    public IveMember(String memberName, int age, String address) {
-        this.memberName = memberName;
-        this.age = age;
-        this.address = address;
-    }
-
-    public String getMemberName() {
-        return memberName;
-    }
-
-    public void setMemberName(String memberName) {
-        this.memberName = memberName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
